8b5417411b97f5a61b8ea474a8dd8a3671af7a05
vitalina moroz <144563@swcsd.us>
2025-05-09 08:15:14 -0400

    Initial Commit

---
+++ Potato/PotatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PotatoApp.swift
+//  Potato
+//
+//  Created by Vitalina Moroz on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PotatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PotatoTests/PotatoTests.swift
@@ -0,0 +1,17 @@
+//
+//  PotatoTests.swift
+//  PotatoTests
+//
+//  Created by Vitalina Moroz on 5/9/25.
+//
+
+import Testing
+@testable import Potato
+
+struct PotatoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
